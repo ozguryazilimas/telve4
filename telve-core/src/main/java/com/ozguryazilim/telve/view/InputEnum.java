e6de3f4251dfa3fa03a2b5ce8c5448ef13ef93c6
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2014-09-05 23:33:08 +0300

    enum tipi bileşenler daha işlevsel hale getirildi

---
+++ telve-core/src/main/java/com/ozguryazilim/telve/view/InputEnum.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.telve.view;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.el.ValueExpression;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UINamingContainer;
+import javax.faces.context.FacesContext;
+
+/**
+ * Telve InpuEnum JSF bileşeni için control sınıfı.
+ *
+ * @author Hakan Uygun
+ */
+@FacesComponent("inputEnum")
+public class InputEnum extends UINamingContainer {
+
+    /**
+     * Verilen Value eğer enum ise onun değer listesini döndürür.
+     *
+     * @return
+     */
+    public List<Object> getItems() {
+
+        List<Object> result = new ArrayList<>();
+
+        Class<?> valueType = getValueType();
+
+        if( (Boolean)getAttributes().get("hasNullValue")){
+            //Null değer girilebilmei için bir null ekliyoruz.
+            result.add(null);
+        }
+
+        if (valueType.isEnum()) {
+            result.addAll(Arrays.asList(valueType.getEnumConstants()));
+        }
+
+        return result;
+    }
+
+    /**
+     * Girilen değerin tipini döndürür.
+     */
+    private Class<?> getValueType() {
+        ValueExpression ve = getValueExpression("value");
+
+        try {
+            return ve.getType(FacesContext.getCurrentInstance().getELContext());
+        } catch (Exception e) {
+            //Do Nothing
+        }
+        return null;
+    }
+
+
+}
