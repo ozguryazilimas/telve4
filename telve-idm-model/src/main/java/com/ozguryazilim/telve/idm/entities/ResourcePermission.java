c2c56f4fcdde78820b543575cef1a6b549fddb50
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2014-09-14 20:11:29 +0300

    PicketLink Simple Modelleri telve-idm-model olarak kopyalandı

--- telve-idm-model/src/main/java/com/ozguryazilim/telve/idm/entities/ResourcePermission.java
+++ telve-idm-model/src/main/java/com/ozguryazilim/telve/idm/entities/ResourcePermission.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ozguryazilim.telve.auth.model;
+package com.ozguryazilim.telve.idm.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
