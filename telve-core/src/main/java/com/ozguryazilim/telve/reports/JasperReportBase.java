78d1ed95ccdbe42c32c61b8596f9910ccb417d07
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2014-11-23 16:36:07 +0200

    JasperReport I18N desteği

--- telve-core/src/main/java/com/ozguryazilim/telve/reports/JasperReportBase.java
+++ telve-core/src/main/java/com/ozguryazilim/telve/reports/JasperReportBase.java
@@ -5,15 +5,21 @@
  */
 package com.ozguryazilim.telve.reports;
 
+import com.ozguryazilim.telve.config.LocaleSelector;
 import com.ozguryazilim.telve.messages.FacesMessages;
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
 import javax.inject.Inject;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRParameter;
 import org.apache.deltaspike.core.api.config.view.ViewConfig;
 import org.apache.deltaspike.core.api.config.view.metadata.ViewConfigResolver;
 import org.primefaces.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * JasperReport raporları için Controller Base sınıfı.
@@ -22,6 +28,8 @@ import org.primefaces.context.RequestContext;
  */
 public abstract class JasperReportBase implements ReportController, Serializable {
 
+    private static final Logger LOG = LoggerFactory.getLogger(JasperReportBase.class);
+    
     @Inject
     private ViewConfigResolver viewConfigResolver;
 
@@ -88,9 +96,11 @@ public abstract class JasperReportBase implements ReportController, Serializable
             
             Map<String, Object> params = new HashMap<>();
             if( buildParam(params) ){
+                decorateParams( params );
                 jasperReportHandler.reportToPDF( getTemplateName(), getTemplateName(), params );
             }
         } catch (JRException ex) {
+            LOG.error("JasperReport Error", ex);
             FacesMessages.error(ex.getMessage());
         }
     }
@@ -121,4 +131,37 @@ public abstract class JasperReportBase implements ReportController, Serializable
         }
         return s;
     }
+    
+    /**
+     * Jasper rapor parametrelerini sistem tarafından gelen değerlerle zenginleştirir.
+     * 
+     * @param params 
+     */
+    protected void decorateParams( Map<String, Object > params ){
+       decorateI18NParams( params ); 
+    }
+    
+    /**
+     * Jasper Report I18N desteği eklenir.
+     * 
+     * @param params 
+     */
+    protected void decorateI18NParams( Map<String, Object > params ){
+        
+        //Önce Resource Bundle adını öğrenelim
+        String s = this.getClass().getAnnotation(Report.class).resource();
+        if( s.isEmpty() ){
+            //Eğer Resorce tanımlanmamışsa template ile aynı ismi kullanıyoruz.
+            s = getTemplateName();
+        }
+        
+        //Sonra resource bundle'ı bağlayalım,
+        Locale locale = LocaleSelector.instance().getLocale();
+        params.put(JRParameter.REPORT_RESOURCE_BUNDLE, ResourceBundle.getBundle(s, locale ));
+        
+        //Şimdide Locele bilgisini bağlayaalım...
+        params.put(JRParameter.REPORT_LOCALE, locale );
+        
+    }
+    
 }
