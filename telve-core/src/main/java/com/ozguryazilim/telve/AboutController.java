273a351fd619219ea9970be0806621a365984da5
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2015-05-01 15:41:16 +0300

    About dialogu düzenlendi

---
+++ telve-core/src/main/java/com/ozguryazilim/telve/AboutController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.telve;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import org.primefaces.context.RequestContext;
+
+/**
+ * About Dialogu için UI controller.
+ * 
+ * @author Hakan Uygun
+ */
+@SessionScoped
+@Named
+public class AboutController implements Serializable{
+    
+    public void openDialog(){
+            
+        Map<String, Object> options = new HashMap<>();
+        options.put("modal", true);
+        //options.put("draggable", false);  
+        options.put("resizable", false);
+        options.put("contentHeight", 450);
+        
+        RequestContext.getCurrentInstance().openDialog("/layout/aboutPopup", options, null);
+    }
+    
+    public void closeDialog() {
+        RequestContext.getCurrentInstance().closeDialog(null);
+    }
+    
+}
