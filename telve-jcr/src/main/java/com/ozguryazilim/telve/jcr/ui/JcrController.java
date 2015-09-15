51be705da92f33a0bb068425ef1286cc2b802a1a
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2015-09-15 16:41:52 +0300

    dosya isimleri için encoder eklendi.

--- telve-jcr/src/main/java/com/ozguryazilim/telve/jcr/ui/JcrController.java
+++ telve-jcr/src/main/java/com/ozguryazilim/telve/jcr/ui/JcrController.java
@@ -22,6 +22,7 @@ import javax.jcr.Session;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.deltaspike.core.api.provider.BeanProvider;
 import org.apache.tika.io.IOUtils;
+import org.modeshape.common.text.UrlEncoder;
 import org.modeshape.jcr.api.JcrTools;
 import org.primefaces.event.FileUploadEvent;
 import org.slf4j.Logger;
@@ -29,31 +30,35 @@ import org.slf4j.LoggerFactory;
 
 /**
  * JCR Arayüzü için control sınıfı.
- * 
+ *
  * Kullanımı için Home beanler üzerinden init edilmesi gerekir.
- * 
- * @author Hakan Uygun 
+ *
+ * @author Hakan Uygun
  */
 public class JcrController {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(JcrController.class);
-    
+
     private String contextRoot;
     private String sourceDomain;
     private String sourceCaption;
     private Long sourceId;
 
-    
     private String selectedPath;
     private String selectedId;
     private String newFolderName;
     private String viewMode = "widget";
-    
+
     private List<Node> folders;
     private List<FileInfo> files;
-    private Map<String,String> parentMap = new HashMap<>();
-    
+    private Map<String, String> parentMap = new HashMap<>();
+
     public JcrController(String contextRoot, String sourceDomain, String sourceCaption, Long sourceId) {
+
+        UrlEncoder encoder = new UrlEncoder();
+        encoder.setSlashEncoded(false);
+        contextRoot = encoder.encode(contextRoot);
+
         this.contextRoot = contextRoot;
         this.sourceDomain = sourceDomain;
         this.sourceCaption = sourceCaption;
@@ -92,10 +97,10 @@ public class JcrController {
     public void setSourceId(Long sourceId) {
         this.sourceId = sourceId;
     }
-    
-    public List<Node> getFolders() throws RepositoryException{
-        if( folders == null ){
-            
+
+    public List<Node> getFolders() throws RepositoryException {
+        if (folders == null) {
+
             populateFolders();
         }
         return folders;
@@ -103,14 +108,14 @@ public class JcrController {
 
     private void populateFolders() throws RepositoryException {
         folders = new ArrayList<>();
-        
+
         Session session = getSession();
         JcrTools jcrTools = new JcrTools();
         Node node = jcrTools.findOrCreateNode(session, getContextRoot(), "nt:folder");
 
-        popuplateFolderNodes( node );
+        popuplateFolderNodes(node);
     }
-    
+
     private void popuplateFolderNodes(Node node) throws RepositoryException {
         folders.add(node);
         parentMap.put(node.getIdentifier(), node.getParent().getIdentifier());
@@ -122,28 +127,28 @@ public class JcrController {
             }
         }
     }
-    
+
     /**
      * Geriye JCR Session instance'ı döndürür.
-     * @return 
+     *
+     * @return
      */
     private Session getSession() {
         return BeanProvider.getContextualReference(Session.class);
     }
 
-    public String getParentIdentifier( String id ){
+    public String getParentIdentifier(String id) {
         return parentMap.get(id);
     }
-    
-    
-    public List<FileInfo> getFiles() throws RepositoryException{
-        if( files == null ){
+
+    public List<FileInfo> getFiles() throws RepositoryException {
+        if (files == null) {
             populateFiles();
         }
-        
+
         return files;
     }
-    
+
     /**
      * Buraya nt:file tipinde node gelir ve fileInfo döner.
      *
@@ -159,27 +164,26 @@ public class JcrController {
         fm.setCreateBy(node.getProperty("jcr:createdBy").getString());
         fm.setCreateDate(node.getProperty("jcr:created").getDate().getTime());
 
-        
         if (node.isNodeType("tlv:ref")) {
             fm.setSourceDomain(node.getProperty("tlv:sourceDomain").getString());
             fm.setSourceCaption(node.getProperty("tlv:sourceCaption").getString());
             fm.setSourceId(node.getProperty("tlv:sourceId").getLong());
-            
+
         }
-        
+
         if (node.isNodeType("tlv:tag")) {
-                        
-            if( node.hasProperty("tlv:info") ){
+
+            if (node.hasProperty("tlv:info")) {
                 fm.setInfo(node.getProperty("tlv:info").getString());
             }
-            
-            if( node.hasProperty("tlv:category") ){
+
+            if (node.hasProperty("tlv:category")) {
                 fm.setCategory(node.getProperty("tlv:category").getString());
             }
             //TODO: Taglar için sanırım array almak lazım
             //fm.setTags(node.getProperty("tlv:tags").getString());
         }
-        
+
         Node cn = node.getNode("jcr:content");
 
         fm.setMimeType(cn.getProperty("jcr:mimeType").getString());
@@ -190,19 +194,19 @@ public class JcrController {
     private void populateFiles() throws RepositoryException {
         populateFiles(getSelectedPath());
     }
-    
-    private void populateFiles( String folder ) throws RepositoryException {
+
+    private void populateFiles(String folder) throws RepositoryException {
         files = new ArrayList<>();
-        
+
         Session session = getSession();
         JcrTools jcrTools = new JcrTools();
         Node node = jcrTools.findOrCreateNode(session, folder, "nt:folder");
 
-        popuplateFileNodes( node );
+        popuplateFileNodes(node);
     }
 
     private void popuplateFileNodes(Node node) throws RepositoryException {
-        
+
         NodeIterator it = node.getNodes();
         while (it.hasNext()) {
             Node n = it.nextNode();
@@ -211,16 +215,15 @@ public class JcrController {
             }
         }
     }
-    
+
     public void newFolder() throws RepositoryException {
         newFolder(newFolderName);
         newFolderName = "";
     }
-    
-    public void newFolder( String folderName ) throws RepositoryException {
+
+    public void newFolder(String folderName) throws RepositoryException {
         LOG.info("Folder Name: {}", folderName);
 
-        
         Session session = getSession();
 
         JcrTools jcrTools = new JcrTools();
@@ -228,10 +231,10 @@ public class JcrController {
 
         folders.add(folder);
         parentMap.put(folder.getIdentifier(), folder.getParent().getIdentifier());
-        
+
         setSelectedPath(folder.getPath());
         setSelectedId(folder.getIdentifier());
-        
+
         LOG.info("Folder Node: {}", folder);
 
         session.save();
@@ -239,7 +242,7 @@ public class JcrController {
         //FacesMessage msg = new FacesMessage("Success! ", folderName + " is created.");
         //FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-    
+
     public void handleFileUpload(FileUploadEvent event) throws RepositoryException {
         LOG.info("Uploaded File : {}", event.getFile().getFileName());
 
@@ -260,18 +263,57 @@ public class JcrController {
         //FacesMessage msg = new FacesMessage("Success! ", fileName + " is uploaded.");
         //FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-    
-    
+
+    /**
+     * path'i ile birlikte verilen fileName'e verilen streami yükler.
+     *
+     * Path'in önüne contextRoot ekler.
+     *
+     * @param fileName
+     * @param in
+     */
+    public void uploadFile(String fileName, InputStream in) {
+        Session session = getSession();
+        try {
+            JcrTools jcrTools = new JcrTools();
+            UrlEncoder encoder = new UrlEncoder();
+            encoder.setSlashEncoded(false);
+            fileName = encoder.encode(fileName);
+
+            LOG.debug("Encoded FileName : {}", fileName);
+            String fullName = getContextRoot() + fileName;
+
+            Node n = jcrTools.uploadFile(session, fullName, in);
+            n.addMixin("tlv:ref");
+            n.addMixin("tlv:tag");
+            n.setProperty("tlv:sourceCaption", getSourceCaption());
+            n.setProperty("tlv:sourceDomain", getSourceDomain());
+            n.setProperty("tlv:sourceId", getSourceId());
+            session.save();
+            //FIXME: Burada aktif kullanıcı adı UserLookup'dan alınacak. Eğer yoksa ( session yoksa yoktur ) anonim bırkılacak.
+            //n.getProperty("jcr:createdBy").setValue("Hakan");
+            //session.save();
+            LOG.debug("Dosya JCR'e kondu : {}", fullName);
+        } catch (RepositoryException ex) {
+            LOG.error("Reporsitory Exception", ex);
+        } catch (IOException ex) {
+            LOG.error("IO Exception", ex);
+        }
+    }
+
     public void copyFile(String fileName, InputStream in) {
-        
+
         Session session = getSession();
 
         try {
             JcrTools jcrTools = new JcrTools();
+
+            UrlEncoder encoder = new UrlEncoder();
+            encoder.setSlashEncoded(false);
+            fileName = encoder.encode(fileName);
+
             Node n = jcrTools.uploadFile(session, fileName, in);
-            
-            
-            
+
             n.addMixin("tlv:ref");
             n.addMixin("tlv:tag");
             n.setProperty("tlv:sourceCaption", getSourceCaption());
@@ -279,18 +321,15 @@ public class JcrController {
             n.setProperty("tlv:sourceId", getSourceId());
 
             session.save();
-            
-            
+
             //FIXME: Burada aktif kullanıcı adı UserLookup'dan alınacak. Eğer yoksa ( session yoksa yoktur ) anonim bırkılacak.
             //n.getProperty("jcr:createdBy").setValue("Hakan");
             //session.save();
-
             //View Modele de ekleyelim.
             files.add(buildFileInfo(n));
-            
 
             LOG.info("Dosya JCR'e kondu : {}", fileName);
-            
+
         } catch (RepositoryException ex) {
             LOG.error("Reporsitory Exception", ex);
         } catch (IOException ex) {
@@ -308,7 +347,7 @@ public class JcrController {
     }
 
     public String getSelectedId() throws RepositoryException {
-        if( Strings.isNullOrEmpty(selectedId)){
+        if (Strings.isNullOrEmpty(selectedId)) {
             Session session = getSession();
             Node node = session.getNode(getSelectedPath());
             selectedId = node.getIdentifier();
@@ -319,7 +358,7 @@ public class JcrController {
     public void setSelectedId(String selectedId) {
         this.selectedId = selectedId;
     }
-    
+
     public void downloadFile(String id) throws RepositoryException {
         Session session = getSession();
 
@@ -338,7 +377,7 @@ public class JcrController {
 
             try (OutputStream out = response.getOutputStream()) {
                 IOUtils.copy(content.getProperty("jcr:data").getBinary().getStream(), out);
-                
+
                 out.flush();
             }
 
@@ -348,7 +387,7 @@ public class JcrController {
             //facesMessages.add("Error while downloading the file: " + fileName + type );
         }
     }
-    
+
     /**
      * Verilen mimeType için tanımlı icon path'ini döndürür.
      *
@@ -358,30 +397,37 @@ public class JcrController {
      * @return
      */
     public String getIcon(String mimeType) {
-        switch( mimeType ){
-            case "application/pdf" : return "fa-file-pdf-o";
-            case "image/png" : return "fa-file-image-o";
-            case "image/jpg" : return "fa-file-image-o";
-            case "image/jpeg" : return "fa-file-image-o";
-            case "text/plain" : return "fa-file-text-o";
-            case "application/msword" :
-            case "application/vnd.oasis.opendocument.text" : return "fa-file-word-o";
+        switch (mimeType) {
+            case "application/pdf":
+                return "fa-file-pdf-o";
+            case "image/png":
+                return "fa-file-image-o";
+            case "image/jpg":
+                return "fa-file-image-o";
+            case "image/jpeg":
+                return "fa-file-image-o";
+            case "text/plain":
+                return "fa-file-text-o";
+            case "application/msword":
+            case "application/vnd.oasis.opendocument.text":
+                return "fa-file-word-o";
             case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-            case "application/vnd.ms-excel" :
-            case "application/vnd.oasis.opendocument.spreadsheet": return "fa-file-excel-o";
+            case "application/vnd.ms-excel":
+            case "application/vnd.oasis.opendocument.spreadsheet":
+                return "fa-file-excel-o";
 
-            default: return "fa-file-o";
+            default:
+                return "fa-file-o";
         }
     }
-    
-    
+
     public void selectNode() throws RepositoryException {
         Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
         String id = params.get("nodeId");
         Session session = getSession();
 
         Node node = session.getNodeByIdentifier(id);
-        
+
         setSelectedPath(node.getPath());
         selectedId = id;
         populateFiles();
@@ -394,7 +440,7 @@ public class JcrController {
     public void setNewFolderName(String newFolderName) {
         this.newFolderName = newFolderName;
     }
-    
+
     public void deleteFile(String id) throws RepositoryException {
         Session session = getSession();
 
@@ -412,11 +458,11 @@ public class JcrController {
             }
         }
     }
-    
+
     public void deleteFolder() throws RepositoryException {
         deleteFolder(getSelectedPath());
     }
-    
+
     public void deleteFolder(String path) throws RepositoryException {
         Session session = getSession();
 
@@ -431,12 +477,12 @@ public class JcrController {
         setSelectedPath(getContextRoot());
         setSelectedId("");
         /*
-        for (Node m : folders) {
-            if (id.equals(m.getIdentifier())) {
-                folders.remove(m);
-                break;
-            }
-        }*/
+         for (Node m : folders) {
+         if (id.equals(m.getIdentifier())) {
+         folders.remove(m);
+         break;
+         }
+         }*/
     }
 
     public String getViewMode() {
@@ -446,10 +492,9 @@ public class JcrController {
     public void setViewMode(String viewMode) {
         this.viewMode = viewMode;
     }
-    
-    
-    public Boolean getIsViewModeWidget(){
+
+    public Boolean getIsViewModeWidget() {
         return "widget".equals(viewMode);
     }
-    
+
 }
