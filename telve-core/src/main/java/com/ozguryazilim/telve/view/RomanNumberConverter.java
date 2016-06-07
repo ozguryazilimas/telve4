b20fb851a670d8b7238f66e8ab5a07d58f099c5d
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2016-06-07 16:12:11 +0300

    Roma Rakamı Desteği eklendi

---
+++ telve-core/src/main/java/com/ozguryazilim/telve/view/RomanNumberConverter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.telve.view;
+
+import com.ozguryazilim.telve.utils.RomanNumeral;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ * Integer değerleri roman rakamı olarak convert eder.
+ * 
+ * @author Hakan Uygun
+ */
+@FacesConverter("romanNumberConverter")
+public class RomanNumberConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        RomanNumeral instance = new RomanNumeral(string);
+        return instance.toInt();
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        if( o instanceof Integer){
+            RomanNumeral instance = new RomanNumeral((Integer)0);
+            return instance.toString();
+        } 
+        
+        return "";
+    }
+    
+}
---
+++ telve-core/src/test/java/com/ozguryazilim/telve/utils/RomanNumeralTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.telve.utils;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author oyas
+ */
+public class RomanNumeralTest {
+    
+
+    /**
+     * Test of toString method, of class RomanNumeral.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        RomanNumeral instance = new RomanNumeral(123);
+        String expResult = "CXXIII";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of toInt method, of class RomanNumeral.
+     */
+    @Test
+    public void testToInt() {
+        System.out.println("toInt");
+        RomanNumeral instance = new RomanNumeral("CXXIII");
+        int expResult = 123;
+        int result = instance.toInt();
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of toString method, of class RomanNumeral.
+     */
+    @Test
+    public void testConverter() {
+        System.out.println("converter");
+        
+        String expResult = "CXXIII";
+        String result = RomanNumeral.integerToRomanNumeral(123);
+        assertEquals(expResult, result);
+        
+    }
+    
+}
