6568902664c881b85a100946b9b0d28d459243da
Erkin ESKİER <wtf@erkinbaba.com>
2016-06-24 19:04:21 +0200

    Todo process ekranı düzenlendi

--- telve-bpm/src/main/java/com/ozguryazilim/telve/bpm/todo/TodoProcess.java
+++ telve-bpm/src/main/java/com/ozguryazilim/telve/bpm/todo/TodoProcess.java
@@ -5,14 +5,17 @@
  */
 package com.ozguryazilim.telve.bpm.todo;
 
-import com.ozguryazilim.telve.auth.UserInfo;
-import com.ozguryazilim.telve.bpm.handlers.AbstractDialogProcessHandler;
-import com.ozguryazilim.telve.bpm.handlers.ProcessHandler;
-import com.ozguryazilim.telve.sequence.SequenceManager;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
 import javax.inject.Inject;
+
+import com.ozguryazilim.telve.auth.UserInfo;
+import com.ozguryazilim.telve.bpm.handlers.AbstractDialogProcessHandler;
+import com.ozguryazilim.telve.bpm.handlers.ProcessHandler;
+import com.ozguryazilim.telve.config.TelveConfigResolver;
+import com.ozguryazilim.telve.sequence.SequenceManager;
 
 /**
  * Bir kişinin kendi kendine atadığı tek kişilik HumanTask süreci.
@@ -24,11 +27,14 @@ public class TodoProcess extends AbstractDialogProcessHandler{
 
     private String subject;
     private String info;
-    private Date startDate;
+    private Date startDate = new Date();
     private Date endDate;
     
     @Inject
     private SequenceManager sequenceManager;
+    
+    @Inject
+    private TelveConfigResolver telveConfigResolver;
     
     @Inject
     private UserInfo userInfo;
@@ -51,6 +57,8 @@ public class TodoProcess extends AbstractDialogProcessHandler{
         values.put("START_DATE", startDate);
         values.put("END_DATE", endDate);
         values.put("ASSIGNEE", userInfo.getLoginName());
+        values.put("AppTitle", telveConfigResolver.getProperty("app.title"));
+        values.put("LinkDomain", telveConfigResolver.getProperty("app.linkDomain"));
         
         String bizKey = userInfo.getLoginName() + "-" + sequenceManager.getNewNumber("todo"+ userInfo.getLoginName(), 5);
         
--- telve-bpm/src/main/java/com/ozguryazilim/telve/bpm/todo/TodoTask.java
+++ telve-bpm/src/main/java/com/ozguryazilim/telve/bpm/todo/TodoTask.java
@@ -5,6 +5,8 @@
  */
 package com.ozguryazilim.telve.bpm.todo;
 
+import java.util.Date;
+
 import com.ozguryazilim.telve.bpm.handlers.AbstractHumanTaskHandler;
 import com.ozguryazilim.telve.bpm.handlers.HumanTaskHandler;
 
@@ -15,6 +17,11 @@ import com.ozguryazilim.telve.bpm.handlers.HumanTaskHandler;
 @HumanTaskHandler(taskName = "todoTask", icon = "fa-check-square-o")
 public class TodoTask extends AbstractHumanTaskHandler{
     
+    private String subject;
+    private String info;
+    private Date startDate;
+    private Date endDate;
+    
     @Override
     public String getDialogName() {
         return "/bpm/todoTaskPopup";
@@ -24,5 +31,44 @@ public class TodoTask extends AbstractHumanTaskHandler{
     public String getViewId() {
         return "/bpm/todoTaskPopup.xhtml";
     }
+
+    @Override
+    protected void popVariables() {
+	subject = (String) getTask().getVariables().get("SUBJECT");
+	info = (String) getTask().getVariables().get("INFO");
+	startDate = (Date)getTask().getVariables().get("START_DATE");
+	endDate = (Date)getTask().getVariables().get("END_DATE");
+    }
     
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Date getStartDate() {
+	return startDate; 
+    }
+
+    public void setStartDate(Date startDate) {
+	this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+	return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+	this.endDate = endDate;
+    }
 }
