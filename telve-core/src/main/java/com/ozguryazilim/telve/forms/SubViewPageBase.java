24fe884ff72ac371ab619944a1b1b0368884169d
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2016-07-04 13:29:26 +0300

    SubViewPageBase change listener yapıyor

--- telve-core/src/main/java/com/ozguryazilim/telve/forms/SubViewPageBase.java
+++ telve-core/src/main/java/com/ozguryazilim/telve/forms/SubViewPageBase.java
@@ -6,6 +6,11 @@
 package com.ozguryazilim.telve.forms;
 
 import java.io.Serializable;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import org.apache.deltaspike.core.api.config.view.ViewConfig;
+import org.apache.deltaspike.core.api.config.view.metadata.ViewConfigResolver;
+import org.apache.deltaspike.core.util.ProxyUtils;
 
 /**
  * Fazla veri girişi ve yetkilendirme problemleri olan kart ve benzeri veri giriş alanları için ek sayfa tanımı olanağı sunar.
@@ -15,6 +20,29 @@ import java.io.Serializable;
  * 
  * @author Hakan Uygun
  */
-public class SubViewPageBase implements Serializable{
+public abstract class SubViewPageBase implements Serializable{
+
+    public abstract void reload();
     
+    @Inject
+    private ViewConfigResolver viewConfigResolver;
+    
+    /**
+     * Eğer bu subview'ın seçildiğine dair FormBase'den event geldiyse tekrar sorgu çek.
+     * @param event 
+     */
+    public void selectionListener( @Observes SubViewSelectEvent event ){
+        String s = viewConfigResolver.getViewConfigDescriptor(getViewPage()).getViewId();
+        if( s.equals(event.getSubViewId())){
+            reload();
+        }
+    }
+
+    /**
+     * Bu SubView için tanımlı viewPage'i döndürür.
+     * @return 
+     */
+    public Class<? extends ViewConfig> getViewPage(){
+        return ((SubView)(ProxyUtils.getUnproxiedClass(this.getClass()).getAnnotation(SubView.class))).viewPage();
+    }
 }
