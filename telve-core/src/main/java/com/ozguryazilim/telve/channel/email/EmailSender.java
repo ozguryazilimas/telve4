3d45f7b59e8a4042ee9c94ec27f3543504e2b469
Erkin ESKİER <erkin.eskier@iova.com.tr>
2018-01-14 16:07:03 +0300

    EMailSender sınıfında null kontrolleri eklendi

--- telve-core/src/main/java/com/ozguryazilim/telve/channel/email/EmailSender.java
+++ telve-core/src/main/java/com/ozguryazilim/telve/channel/email/EmailSender.java
@@ -5,10 +5,9 @@
  */
 package com.ozguryazilim.telve.channel.email;
 
-import com.google.common.base.Splitter;
-import com.ozguryazilim.telve.audit.AuditLogger;
 import java.util.List;
 import java.util.Map;
+
 import javax.activation.DataHandler;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
@@ -25,7 +24,12 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+
 import org.apache.deltaspike.core.api.config.ConfigResolver;
+
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.ozguryazilim.telve.audit.AuditLogger;
 
 /**
  * Java EE Mail Session üzerinden e-posta atmak için API.
@@ -44,7 +48,7 @@ public class EmailSender {
 
     @Inject
     private AuditLogger auditLogger;
-    
+
     /**
      * Verilen bilgilerle sunucu üzerine tanımlanmış ayarlar ile e-posta atar.
      *
@@ -62,19 +66,19 @@ public class EmailSender {
         message.setSubject(subject);
         message.setContent(body, "text/plain; charset=UTF-8");
         Transport.send(message);
-        
+
         auditLogger.actionLog("NOTIFICATION", 0l, to, "EMAIL", "SEND", "SYSTEM", subject);
     }
 
     /**
      * Attachment'ları ekleyerek multipart e-posta atar.
-     * 
+     *
      * @param from
      * @param to
      * @param subject
      * @param body
      * @param attachments
-     * @throws MessagingException 
+     * @throws MessagingException
      */
     public void send(String from, String to, String subject, String body, Map<String, DataHandler> attachments) throws MessagingException {
         Message message = new MimeMessage(mailSession);
@@ -102,17 +106,17 @@ public class EmailSender {
         }
 
         message.setContent(multipart);
-        
+
         Transport.send(message);
-        
+
         auditLogger.actionLog("NOTIFICATION", 0l, to, "EMAIL", "SEND", "SYSTEM", subject);
     }
 
-    
-    
+
+
     /**
      * Attachment'ları ekleyerek multipart e-posta atar.
-     * 
+     *
      * @param from
      * @param to Ad Soyad <adres@domain.com>, ... formatında
      * @param cc
@@ -120,34 +124,41 @@ public class EmailSender {
      * @param subject
      * @param body
      * @param attachments
-     * @throws MessagingException 
+     * @throws MessagingException
      */
     public void send(String from, String to, String cc, String bcc, String subject, String body, Map<String, DataHandler> attachments) throws MessagingException {
         Message message = new MimeMessage(mailSession);
         message.setFrom(new InternetAddress(from));
-        
+
         //To addresses
         List<String> toAddrs = Splitter.on(',').trimResults().trimResults().splitToList(to);
         for( String a : toAddrs ){
             Address toAddress = new InternetAddress(a);
             message.addRecipient(Message.RecipientType.TO, toAddress);
         }
-        
+
         //CC addresses
-        List<String> ccAddrs = Splitter.on(',').trimResults().trimResults().splitToList(cc);
-        for( String cca : ccAddrs ){
-            Address ccAddress = new InternetAddress(cca);
-            message.addRecipient(Message.RecipientType.CC, ccAddress);
+        if(!Strings.isNullOrEmpty(cc)) {
+            List<String> ccAddrs = Splitter.on(',').trimResults().trimResults().splitToList(cc);
+            for( String cca : ccAddrs ){
+        	if(!Strings.isNullOrEmpty(cca)) {
+        	    Address ccAddress = new InternetAddress(cca);
+        	    message.addRecipient(Message.RecipientType.CC, ccAddress);
+        	}
+            }
         }
-        
+
         //BCC addresses
-        List<String> bccAddrs = Splitter.on(',').trimResults().trimResults().splitToList(bcc);
-        for( String bcca : ccAddrs ){
-            Address bccAddress = new InternetAddress(bcca);
-            message.addRecipient(Message.RecipientType.BCC, bccAddress);
+        if(!Strings.isNullOrEmpty(bcc)) {
+            List<String> bccAddrs = Splitter.on(',').trimResults().trimResults().splitToList(bcc);
+            for( String bcca : bccAddrs ){
+        	if(!Strings.isNullOrEmpty(bcca)) {
+        	    Address bccAddress = new InternetAddress(bcca);
+        	    message.addRecipient(Message.RecipientType.BCC, bccAddress);
+        	}
+            }
         }
-               
-        
+
         message.setSubject(subject);
 
         // Create the message part
@@ -171,28 +182,28 @@ public class EmailSender {
         }
 
         message.setContent(multipart);
-        
+
         Transport.send(message);
-        
+
         auditLogger.actionLog("NOTIFICATION", 0l, to, "EMAIL", "SEND", "SYSTEM", subject);
     }
-    
+
     /**
      * From bilgisini ayarlardan okur.
-     * 
+     *
      * @param to
      * @param cc
      * @param bcc
      * @param subject
      * @param body
      * @param attachments
-     * @throws MessagingException 
+     * @throws MessagingException
      */
     public void send(String to, String cc, String bcc, String subject, String body, Map<String, DataHandler> attachments) throws MessagingException {
         String from = ConfigResolver.getProjectStageAwarePropertyValue("app.email.from");
         send(from, to, cc, bcc, subject, body, attachments);
     }
-    
+
     /**
      * From bilgisini ayarlardan okuyarak e-posta atar.
      *
@@ -205,15 +216,15 @@ public class EmailSender {
         String from = ConfigResolver.getProjectStageAwarePropertyValue("app.email.from");
         send(from, to, subject, body);
     }
-    
+
     /**
      * From bilgisini ayarlardan okuyarak e-posta atar.
-     * 
+     *
      * @param to
      * @param subject
      * @param body
      * @param attachments
-     * @throws MessagingException 
+     * @throws MessagingException
      */
     public void send(String to, String subject, String body, Map<String, DataHandler> attachments) throws MessagingException {
         String from = ConfigResolver.getProjectStageAwarePropertyValue("app.email.from");
