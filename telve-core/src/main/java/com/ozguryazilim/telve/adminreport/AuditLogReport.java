02b522e517d9a23bc08f0f23e05b7a145c4fafdf
Erkin ESKİER <erkin.eskier@iova.com.tr>
2018-04-02 17:59:03 +0300

    Rapor dil dosyaları asıl dil dosyalarıyla birleştirildi.
    Rapor RESOURCE_BUNDLE ayarları düzenlendi

--- telve-core/src/main/java/com/ozguryazilim/telve/adminreport/AuditLogReport.java
+++ telve-core/src/main/java/com/ozguryazilim/telve/adminreport/AuditLogReport.java
@@ -1,17 +1,16 @@
 /*
  *   Copyleft 2013 IOVA SOFTWARE
- *   
+ *
  *  Distributable under LGPL license.
  *  See terms of license at gnu.org.
  *  http://www.gnu.org/licenses/lgpl.html
- *   
+ *
  *  www.openohs.com
  *  www.iova.com.tr
  */
 
 package com.ozguryazilim.telve.adminreport;
 
-import com.ozguryazilim.telve.adminreport.AuditLogFilter;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Map;
@@ -21,29 +20,26 @@ import javax.inject.Inject;
 
 import org.joda.time.DateTime;
 
-
 import com.ozguryazilim.telve.config.TelveConfigResolver;
-import com.ozguryazilim.telve.messages.TelveResourceBundle;
 import com.ozguryazilim.telve.query.filters.DateValueType;
 import com.ozguryazilim.telve.reports.JasperReportBase;
 import com.ozguryazilim.telve.reports.Report;
 import com.ozguryazilim.telve.reports.ReportDate;
 import com.ozguryazilim.telve.view.Pages;
-import net.sf.jasperreports.engine.JRParameter;
 
 /**
  * AuditLog'lar için standart rapor
- * 
+ *
  * @author Hakan Uygun
  */
-@Report( filterPage = Pages.Admin.AdminReportPages.AuditLogReport.class, permission="auditLogReport", path="/admin/audit", template = "auditLogReport", resource = "adminReports")
+@Report( filterPage = Pages.Admin.AdminReportPages.AuditLogReport.class, permission="auditLogReport", path="/admin/audit", template = "auditLogReport")
 public class AuditLogReport extends JasperReportBase{
 
     @Inject
     private TelveConfigResolver telveConfigResolver;
-    
+
     private AuditLogFilter filter;
-    
+
     public AuditLogFilter getFilter() {
 	if (filter == null) {
             buildFilter();
@@ -68,7 +64,7 @@ public class AuditLogReport extends JasperReportBase{
     protected boolean buildParam(Map<String, Object> params) {
 	params.put("BEGIN_DATE", getFilter().getBeginDate());
         params.put("END_DATE", getFilter().getEndDate());
-        
+
         String logo = telveConfigResolver.getProperty("brand.company.reportLogo");
         String title = telveConfigResolver.getProperty("brand.company.reportTitle");
 
@@ -83,16 +79,7 @@ public class AuditLogReport extends JasperReportBase{
 	    }
         }
         params.put("FIRM_TITLE", title);
-        
+
         return true;
     }
-
-    @Override
-    protected void decorateI18NParams(Map<String, Object> params) {
-        params.put(JRParameter.REPORT_RESOURCE_BUNDLE, TelveResourceBundle.getBundle());
-        super.decorateI18NParams(params); //To change body of generated methods, choose Tools | Templates.           
-    }
-    
-
-    
 }
