9d5c58175890aa99ef0408df42f5247582af97af
scirit <soner.cirit@ozguryazilim.com.tr>
2018-07-02 18:11:58 +0300

    refs #29406 parolami unuttum destegi icin token olusturma ve email ile yollama

--- telve-idm/src/main/java/com/ozguryazilim/telve/idm/user/ForgotPassword.java
+++ telve-idm/src/main/java/com/ozguryazilim/telve/idm/user/ForgotPassword.java
@@ -1,11 +1,32 @@
 package com.ozguryazilim.telve.idm.user;
 
+import com.google.common.hash.Hashing;
+import com.ozguryazilim.telve.channel.email.EmailChannel;
+import com.ozguryazilim.telve.config.TelveConfigResolver;
+import com.ozguryazilim.telve.idm.entities.User;
+import com.ozguryazilim.telve.messages.FacesMessages;
+import com.ozguryazilim.telve.messages.Messages;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.NoResultException;
 
 @RequestScoped
 @Named
 public class ForgotPassword {
+
+    @Inject
+    private UserRepository repository;
+
+    @Inject
+    private EmailChannel emailChannel;
+
+    @Inject
+    private TelveConfigResolver resolver;
 
     private String email;
 
@@ -15,5 +36,34 @@ public class ForgotPassword {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String sendEmailWithToken() {
+        try {
+            User user = repository.findByEmail(email);
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("messageClass", "PASSWORDRENEWALTOKEN");
+            params.put("token", generateToken());
+            params.put("entity", user);
+            params.put("telveConfigResolver", resolver);
+
+            emailChannel.sendMessage(email, "", "", params);
+
+            FacesMessages.info(Messages.getMessageFromData(Messages.getCurrentLocale(),
+                    "forgotPassword.info.SentRenewalEmail" + "$%&" + email));
+            return "/login.xhtml?faces-redirect=true";
+        } catch (NoResultException nre) {
+            FacesMessages.error(Messages.getMessage("forgotPassword.error.InvalidEmail"));
+            return null;
+        }
+    }
+
+    private String generateToken() {
+        return Hashing
+                .sha256()
+                .hashString(UUID.randomUUID().toString() + System.currentTimeMillis(),
+                        StandardCharsets.UTF_8)
+                .toString();
     }
 }
--- telve-idm/src/main/resources/freemarker/emailPasswordRenewalToken.ftl
+++ telve-idm/src/main/resources/freemarker/emailPasswordRenewalToken.ftl
@@ -1,1 +1,8 @@
 <#ftl encoding="UTF-8">
+${request.setHeader("subject", headers.telveConfigResolver.getProperty("app.title") + " " + headers.messages["email.subject.PasswordRenewal"])}
+
+${headers.messages["email.body.PasswordRenewal"]}
+
+${headers.telveConfigResolver.getProperty("app.linkDomain")}/passwordrenewal.xhtml?token=${headers.token}
+--
+provided by telve email channel
