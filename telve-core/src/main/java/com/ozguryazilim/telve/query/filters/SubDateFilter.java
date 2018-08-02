4a9506144373291741a69336b6dd653ae0c80d74
Yusuf Unlu <myunlu35@gmail.com>
2018-08-02 16:33:38 +0300

    refs #29738 SubDateFilter ve SubDateColumn yazılması.

---
+++ telve-core/src/main/java/com/ozguryazilim/telve/query/filters/SubDateFilter.java
@@ -0,0 +1,312 @@
+package com.ozguryazilim.telve.query.filters;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.ozguryazilim.telve.query.Operands;
+import com.ozguryazilim.telve.utils.DateUtils;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+import org.apache.deltaspike.data.api.criteria.Criteria;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+/**
+ *
+ * @author yusuf
+ * @param <E> İşlenecek olan entity
+ * @param <D> Alt Entity
+ */
+public class SubDateFilter<E, D> extends Filter<E, D, Date> {
+
+    private static List<DateValueType> inTypes;
+    private static List<DateValueType> betweenTypes;
+
+    private DateValueType valueType = DateValueType.Today;
+    private DateValueType valueType2 = DateValueType.Today;
+
+    private SingularAttribute< ? super D, Date> subattribute;
+
+    public SubDateFilter(SingularAttribute< ? super E, D> attribute, SingularAttribute< ? super D, Date> subattribute, String label) {
+        super(attribute, label);
+
+        setOperands(Operands.getDateOperands());
+        setOperand(FilterOperand.Equal);
+        this.subattribute = subattribute;
+    }
+
+    public SubDateFilter(SingularAttribute< ? super E, D> attribute, SingularAttribute< ? super D, Date> subattribute, String label, FilterOperand operand, DateValueType valueType) {
+        super(attribute, label);
+
+        setOperands(Operands.getDateOperands());
+        setOperand(operand);
+        this.valueType = valueType;
+        this.subattribute = subattribute;
+    }
+
+    @Override
+    public void decorateCriteria(Criteria<E, ?> criteria) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void decorateCriteriaQuery(List<Predicate> predicates, CriteriaBuilder builder, Root<E> from) {
+        calcDates();
+        if (getValue() != null) {
+            switch (getOperand()) {
+                case Equal:
+                    predicates.add(builder.equal(from.get(getAttribute()).get(subattribute), getValue()));
+                    break;
+                case NotEqual:
+                    predicates.add(builder.notEqual(from.get(getAttribute()).get(subattribute), getValue()));
+                    break;
+                case Greater:
+                    predicates.add(builder.greaterThan(from.get(getAttribute()).get(subattribute), getValue()));
+                    break;
+                case GreaterOrEqual:
+                    predicates.add(builder.greaterThanOrEqualTo(from.get(getAttribute()).get(subattribute), getValue()));
+                    break;
+                case Lesser:
+                    predicates.add(builder.lessThan(from.get(getAttribute()).get(subattribute), getValue()));
+                    break;
+                case LesserOrEqual:
+                    predicates.add(builder.lessThanOrEqualTo(from.get(getAttribute()).get(subattribute), getValue()));
+                    break;
+                case In:
+                    predicates.add(builder.between(from.get(getAttribute()).get(subattribute), getValue(), getValue2()));
+                    break;
+                case Between:
+                    predicates.add(builder.between(from.get(getAttribute()).get(subattribute), getValue(), getValue2()));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public String getTemplate() {
+        return "dateFilter";
+    }
+
+    public void calcDates() {
+
+        LocalDate dt = new LocalDate();
+
+        switch (getValueType()) {
+            case Today:
+                setValue(dt.toDate());
+                break;
+            case Tomorrow:
+                setValue(dt.plusDays(1).toDate());
+                break;
+            case Yesterday:
+                setValue(dt.minusDays(1).toDate());
+                break;
+            case FirstDayOfMonth:
+                setValue(dt.dayOfMonth().withMinimumValue().toDate());
+                break;
+            case LastDayOfMonth:
+                setValue(dt.dayOfMonth().withMaximumValue().toDate());
+                break;
+            case FirstDayOfWeek:
+                setValue(dt.dayOfWeek().withMinimumValue().toDate());
+                break;
+            case LastDayOfWeek:
+                setValue(dt.dayOfWeek().withMaximumValue().toDate());
+                break;
+            case FirstDayOfYear:
+                setValue(dt.dayOfYear().withMinimumValue().toDate());
+                break;
+            case LastDayOfYear:
+                setValue(dt.dayOfYear().withMaximumValue().toDate());
+                break;
+            case TenDaysBefore:
+                setValue(dt.minusDays(10).toDate());
+                break;
+            case TenDaysAfter:
+                setValue(dt.plusDays(10).toDate());
+                break;
+            case ThisWeek:
+                setValue(dt.dayOfWeek().withMinimumValue().toDate());
+                setValue2(dt.dayOfWeek().withMaximumValue().toDate());
+                break;
+            case LastWeek:
+                dt = dt.minusWeeks(1);
+                setValue(dt.dayOfWeek().withMinimumValue().toDate());
+                setValue2(dt.dayOfWeek().withMaximumValue().toDate());
+                break;
+            case NextWeek:
+                dt = dt.plusWeeks(1);
+                setValue(dt.dayOfWeek().withMinimumValue().toDate());
+                setValue2(dt.dayOfWeek().withMaximumValue().toDate());
+                break;
+            case ThisMonth:
+                setValue(dt.dayOfMonth().withMinimumValue().toDate());
+                setValue2(dt.dayOfMonth().withMaximumValue().toDate());
+                break;
+            case NextMonth:
+                dt = dt.plusMonths(1);
+                setValue(dt.dayOfMonth().withMinimumValue().toDate());
+                setValue2(dt.dayOfMonth().withMaximumValue().toDate());
+                break;
+            case LastMonth:
+                dt = dt.minusMonths(1);
+                setValue(dt.dayOfMonth().withMinimumValue().toDate());
+                setValue2(dt.dayOfMonth().withMaximumValue().toDate());
+                break;
+            case NextTenDays:
+                setValue(dt.toDate());
+                setValue2(dt.plusDays(10).toDate());
+                break;
+            case LastTenDays:
+                setValue(dt.minusDays(10).toDate());
+                setValue2(dt.toDate());
+                break;
+            case LastOneYear:
+                setValue(dt.minusYears(1).toDate());
+                setValue2(dt.toDate());
+                break;
+            case NextOneYear:
+                setValue(dt.toDate());
+                setValue2(dt.plusYears(1).toDate());
+                break;
+        }
+
+        //Tek günlü değerler için valueType 2 lazım onlara da bakıyoruz.
+        if (FilterOperand.Between.equals(getOperand())) {
+            switch (getValueType2()) {
+                case Today:
+                    setValue2(dt.toDate());
+                    break;
+                case Tomorrow:
+                    setValue2(dt.plusDays(1).toDate());
+                    break;
+                case Yesterday:
+                    setValue2(dt.minusDays(1).toDate());
+                    break;
+                case FirstDayOfMonth:
+                    setValue2(dt.dayOfMonth().withMinimumValue().toDate());
+                    break;
+                case LastDayOfMonth:
+                    setValue2(dt.dayOfMonth().withMaximumValue().toDate());
+                    break;
+                case FirstDayOfWeek:
+                    setValue2(dt.dayOfWeek().withMinimumValue().toDate());
+                    break;
+                case LastDayOfWeek:
+                    setValue2(dt.dayOfWeek().withMaximumValue().toDate());
+                    break;
+                case FirstDayOfYear:
+                    setValue2(dt.dayOfYear().withMinimumValue().toDate());
+                    break;
+                case LastDayOfYear:
+                    setValue2(dt.dayOfYear().withMaximumValue().toDate());
+                    break;
+                case TenDaysBefore:
+                    setValue2(dt.minusDays(10).toDate());
+                    break;
+                case TenDaysAfter:
+                    setValue2(dt.plusDays(10).toDate());
+                    break;
+            }
+        }
+
+    }
+
+    /**
+     * Operanda bakarak olası veri tiplerini geriye döner.
+     *
+     * @return
+     */
+    public List<DateValueType> getValueTypes() {
+
+        if (getOperand() == FilterOperand.In) {
+            if (inTypes == null) {
+                inTypes = new ArrayList<>();
+                inTypes.add(DateValueType.ThisWeek);
+                inTypes.add(DateValueType.ThisMonth);
+                inTypes.add(DateValueType.LastWeek);
+                inTypes.add(DateValueType.LastTenDays);
+                inTypes.add(DateValueType.LastMonth);
+                inTypes.add(DateValueType.NextWeek);
+                inTypes.add(DateValueType.NextTenDays);
+                inTypes.add(DateValueType.NextMonth);
+                inTypes.add(DateValueType.NextOneYear);
+                inTypes.add(DateValueType.LastOneYear);
+            }
+
+            return inTypes;
+
+        } else {
+
+            if (betweenTypes == null) {
+                betweenTypes = new ArrayList<>();
+                betweenTypes.add(DateValueType.Date);
+                betweenTypes.add(DateValueType.Today);
+                betweenTypes.add(DateValueType.Tomorrow);
+                betweenTypes.add(DateValueType.Yesterday);
+                betweenTypes.add(DateValueType.FirstDayOfWeek);
+                betweenTypes.add(DateValueType.FirstDayOfMonth);
+                betweenTypes.add(DateValueType.FirstDayOfYear);
+                betweenTypes.add(DateValueType.LastDayOfWeek);
+                betweenTypes.add(DateValueType.LastDayOfMonth);
+                betweenTypes.add(DateValueType.LastDayOfYear);
+                betweenTypes.add(DateValueType.TenDaysAfter);
+                betweenTypes.add(DateValueType.TenDaysBefore);
+            }
+
+            return betweenTypes;
+        }
+
+    }
+
+    public DateValueType getValueType() {
+        return valueType;
+    }
+
+    public void setValueType(DateValueType valueType) {
+        this.valueType = valueType;
+    }
+
+    public DateValueType getValueType2() {
+        return valueType2;
+    }
+
+    public void setValueType2(DateValueType valueType2) {
+        this.valueType2 = valueType2;
+    }
+
+    @Override
+    public String serialize() {
+        return Joiner.on("::").join(getOperand(), getValue() == null ? "null" : getValue());
+    }
+
+    @Override
+    public void deserialize(String s) {
+        List<String> ls = Splitter.on("::").trimResults().splitToList(s);
+        setOperand(FilterOperand.valueOf(ls.get(0)));
+
+        setValueType(DateValueType.valueOf(ls.get(1)));
+
+        if (!"null".equals(ls.get(2))) {
+            DateTime parseDateTime = DateUtils.getDateTimeFormatter().parseDateTime(ls.get(2));
+            setValue(parseDateTime.toDate());
+        } else {
+            setValue(null);
+        }
+
+        setValueType2(DateValueType.valueOf(ls.get(3)));
+        if (!"null".equals(ls.get(4))) {
+            DateTime parseDateTime = DateUtils.getDateTimeFormatter().parseDateTime(ls.get(4));
+            setValue2(parseDateTime.toDate());
+        } else {
+            setValue2(null);
+        }
+    }
+
+}
