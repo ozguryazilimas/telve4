2e4f85ba9516d25c801cc195622d3dab19be05c5
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2018-12-02 18:55:55 +0300

    refs #31286 Uppy için webjar

---
+++ telve-uppy-webjar/src/main/jsapp/index.js
@@ -0,0 +1,8 @@
+
+module.exports.Core = require('@uppy/core');
+module.exports.Dashboard = require('@uppy/dashboard');
+module.exports.Tus = require('@uppy/tus');
+module.exports.FileInput = require('@uppy/file-input');
+module.exports.ProgressBar = require('@uppy/progress-bar');
+module.exports.Css = require('uppy/dist/uppy.min.css');
+
