2b8bd01efcc05edaf48ee5801f41730e21a80c68
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2018-12-20 13:49:29 +0300

    refs #31488 lisans başlıkları artık otomatik temizlenecek

--- modules/telve-idm/src/main/java/com/ozguryazilim/telve/permisson/PermissionGroup.java
+++ modules/telve-idm/src/main/java/com/ozguryazilim/telve/permisson/PermissionGroup.java
@@ -1,12 +1,3 @@
-/*
- * Copyleft 2007-2010 Uygun Teknoloji
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- * http://www.gnu.org/licenses/lgpl.html
- * 
- * www.uygunteknoloji.com.tr
- */
 package com.ozguryazilim.telve.permisson;
 
 import java.io.Serializable;
