de8bd5c48b85d75b50236b1b4c3ede4983d0295f
scirit <soner.cirit@ozguryazilim.com.tr>
2018-12-27 16:10:46 +0300

    refs #31562 adds locale folder and refactors webpack config

---
+++ modules/telve-uppy-webjar/src/main/jsapp/locale/turkish.js
@@ -0,0 +1,109 @@
+module.exports = {
+    strings: {
+        //core
+        youCanOnlyUploadX: {
+            0: 'Sadece %{smart_count} adet dosya yükleyebilirsiniz',
+            1: 'Sadece %{smart_count} adet dosya yükleyebilirsiniz'
+        },
+        youHaveToAtLeastSelectX: {
+            0: 'En az %{smart_count} adet dosya seçmeniz gereklidir',
+            1: 'En az %{smart_count} adet dosya seçmeniz gereklidir'
+        },
+        exceedsSize: 'Maksimum dosya boyutu:',
+        youCanOnlyUploadFileTypes: 'İzin verilen dosya tipleri:',
+        companionError: 'Bağlantı hatası',
+        failedToUpload: '%{file} dosyası için yükleme başarısız oldu',
+        noInternetConnection: 'İnternet bağlantısı yok',
+        connectedToInternet: 'İnternete bağlanıldı',
+        // Strings for remote providers
+        noFilesFound: 'Burada bir dosya veya klasör yok',
+        selectXFiles: {
+            0: '%{smart_count} adet dosya seç',
+            1: '%{smart_count} adet dosya seç'
+        },
+        cancel: 'İptal et',
+        logOut: 'Çıkış yap',
+        //dashboard
+        selectToUpload: 'Yüklenecek dosyaları seçin',
+        closeModal: 'Açılır pencereyi kapat',
+        upload: 'Yükle',
+        importFrom: 'Şuradan içe aktar: %{name}',
+        addingMoreFiles: 'Dosyalar ekleniyor',
+        addMoreFiles: 'Fazladan dosya ekle',
+        dashboardWindowTitle: 'Uppy Kontrol Paneli Penceresi (Kapatmak için \'escape\' tuşuna basın)',
+        dashboardTitle: 'Uppy Kontrol Paneli',
+        copyLinkToClipboardSuccess: 'Bağlantı panoya kopyalandı',
+        copyLinkToClipboardFallback: 'Aşağıdaki bağlantıyı kopyalayın',
+        copyLink: 'Bağlantıyı kopyala',
+        fileSource: 'Dosya kaynağı: %{name}',
+        done: 'Tamamlandı',
+        back: 'Geri',
+        name: 'İsim',
+        removeFile: 'Dosyayı kaldır',
+        editFile: 'Dosyayı düzenle',
+        editing: 'Düzenleniyor: %{file}',
+        edit: 'Düzenle',
+        finishEditingFile: 'Dosyayı düzenlemeyi bitir',
+        saveChanges: 'Değişiklikleri kaydet',
+        localDisk: 'Yerel Disk',
+        myDevice: 'Benim Cihazım',
+        dropPasteImport: 'Dosyaları buraya sürükleyip bırak, yapıştır, %{browse} veya içe aktar',
+        dropPaste: 'Dosyaları buraya sürükleyip bırak, yapıştır veya %{browse}',
+        browse: 'göz at',
+        fileProgress: 'Dosya ilerleme durumu: yükleme hızı ve tahmini bitiş süresi',
+        numberOfSelectedFiles: 'Seçilen dosya sayısı',
+        uploadAllNewFiles: 'Yeni dosyaların tümünü yükle',
+        emptyFolderAdded: 'Boş klasörden hiçbir dosya eklenmedi',
+        uploadComplete: 'Yükleme tamamlandı',
+        uploadPaused: 'Yükleme duraklatıldı',
+        resumeUpload: 'Yüklemeyi devam ettir',
+        pauseUpload: 'Yüklemeyi duraklat',
+        retryUpload: 'Tekrar yüklemeyi dene',
+        cancelUpload: 'Yüklemeyi iptal et',
+        xFilesSelected: {
+            0: '%{smart_count} adet dosya seçildi',
+            1: '%{smart_count} adet dosya seçildi'
+        },
+        uploadXFiles: {
+            0: '%{smart_count} adet dosya yükle',
+            1: '%{smart_count} adet dosya yükle'
+        },
+        uploadingXFiles: {
+            0: '%{smart_count} adet dosya yükleniyor',
+            1: '%{smart_count} adet dosya yükleniyor'
+        },
+        processingXFiles: {
+            0: '%{smart_count} adet dosya işleniyor',
+            1: '%{smart_count} adet dosya işleniyor'
+        },
+        uploadXNewFiles: {
+            0: '+%{smart_count} adet dosya yükle',
+            1: '+%{smart_count} adet dosya yükle'
+        },
+        folderAdded: {
+            0: '%{folder} klasöründen %{smart_count} dosya eklendi',
+            1: '%{folder} klasöründen %{smart_count} dosya eklendi'
+        },
+        //statusbar
+        uploading: 'Yükleniyor',
+        complete: 'Tamamlandı',
+        uploadFailed: 'Yükleme başarısız oldu',
+        pleasePressRetry: 'Tekrar yüklemek için lütfen \'Yeniden dene\' tuşuna basınız',
+        paused: 'Duraklatıldı',
+        error: 'Hata',
+        retry: 'Yeniden dene',
+        pause: 'Duraklat',
+        resume: 'Devam et',
+        pressToRetry: 'Tekrar denemek için buraya basınız',
+        filesUploadedOfTotal: {
+            0: '%{smart_count} adet dosyadan %{complete} tanesi yüklendi',
+            1: '%{smart_count} adet dosyadan %{complete} tanesi yüklendi'
+        },
+        dataUploadedOfTotal: '%{complete}/%{total}',
+        xTimeLeft: 'Kalan süre: %{time}',
+        xMoreFilesAdded: {
+            0: '%{smart_count} adet dosya eklendi',
+            1: '%{smart_count} adet dosya eklendi'
+        }
+    }
+};
