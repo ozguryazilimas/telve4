d4229b6153d550d7bbb5826c30e83e47a7af3dc0
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2019-04-10 23:02:45 +0300

    refs #32704 uppy sürümü 0.30.4 yükseltildi
    es6-promise paketi webpack'e deneme için tanıtıldı

--- modules/telve-uppy-webjar/webpack.config.js
+++ modules/telve-uppy-webjar/webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const buildPath = path.join(__dirname, 'target/classes/META-INF/resources/webjars', packageJSON.name, packageJSON.version);
 
+var webpack = require("webpack");
+
 module.exports = {
     mode: 'production',
     entry: './src/main/jsapp/index.js',
@@ -14,6 +16,11 @@ module.exports = {
             }
         ]
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+        Promise: ['es6-promise', 'Promise']
+    })
+  ],
   output: {
     path: buildPath,
     filename: 'uppy-bundle.js',
