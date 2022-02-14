ceff95b0db949224c7e027ff682643efe6ec48f0
emrygun <emre6271@gmail.com>
2022-02-14 15:09:55 +0300

    refs #57670 rest api request response log impl

--- modules/telve-core/src/main/java/com/ozguryazilim/telve/config/RequestLoggingFilter.java
+++ modules/telve-core/src/main/java/com/ozguryazilim/telve/config/RequestLoggingFilter.java
@@ -4,26 +4,40 @@ import com.google.gson.Gson;
 import com.ozguryazilim.telve.rest.ext.Logged;
 import com.ozguryazilim.telve.rest.ext.RequestLog;
 import com.ozguryazilim.telve.rest.ext.ResponseLog;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @Logged
 @Provider
-public class RequestLoggingFilter implements ContainerResponseFilter {
+public class RequestLoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private Logger LOG = LoggerFactory.getLogger(RequestLoggingFilter.class);
 
+    private Gson gson = new Gson();
+
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        Gson gson = new Gson();
-        LOG.info(gson.toJson(new RequestLog(requestContext)));
-        LOG.info(gson.toJson(new ResponseLog(responseContext)));
+        LOG.info("Response: " + gson.toJson(new ResponseLog(responseContext)));
 
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        byte[] entityByteArray = IOUtils.toByteArray(requestContext.getEntityStream());
+        LOG.info("Request: " + gson.toJson(new RequestLog(requestContext, entityByteArray)));
+        requestContext.setEntityStream(new ByteArrayInputStream(entityByteArray));
     }
 }
--- modules/telve-core/src/main/java/com/ozguryazilim/telve/rest/ext/RequestLog.java
+++ modules/telve-core/src/main/java/com/ozguryazilim/telve/rest/ext/RequestLog.java
@@ -1,30 +1,37 @@
 package com.ozguryazilim.telve.rest.ext;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.CloseShieldInputStream;
+
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Request;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class RequestLog {
     private String path;
     private MultivaluedMap<String, String> headers;
-    private Request request;
+    private String entity;
 
     public RequestLog() { }
 
-    public RequestLog(String path, MultivaluedMap<String, String> headers, Request request) {
+    public RequestLog(String path, MultivaluedMap<String, String> headers, String entity) {
         this.path = path;
         this.headers = headers;
-        this.request = request;
+        this.entity = entity;
     }
 
-    public RequestLog(ContainerRequestContext requestContext) {
+    public RequestLog(ContainerRequestContext requestContext, byte[] entityByteArray) throws IOException {
         this.path = requestContext.getUriInfo().getPath();
         this.headers = requestContext.getHeaders();
-        this.request = requestContext.getRequest();
+        this.entity = new String(entityByteArray, StandardCharsets.UTF_8);
     }
 
     public String getPath() {
-
         return path;
     }
 
@@ -40,11 +47,11 @@ public class RequestLog {
         this.headers = headers;
     }
 
-    public Request getRequest() {
-        return request;
+    public String getEntity() {
+        return entity;
     }
 
-    public void setRequest(Request request) {
-        this.request = request;
+    public void setEntity(String entity) {
+        this.entity = entity;
     }
 }
