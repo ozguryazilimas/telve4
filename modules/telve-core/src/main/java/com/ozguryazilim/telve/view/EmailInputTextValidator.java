6834c36039066425d1872bdb06d5138e35cc9a9d
emrygun <emre6271@gmail.com>
2023-06-18 20:53:17 +0300

    refs #74444 added email validation to userEdit form

---
+++ modules/telve-core/src/main/java/com/ozguryazilim/telve/view/EmailInputTextValidator.java
@@ -0,0 +1,33 @@
+package com.ozguryazilim.telve.view;
+
+import org.primefaces.component.inputtext.InputText;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.util.regex.Pattern;
+
+@FacesValidator("emailInputTextValidator")
+public class EmailInputTextValidator implements Validator {
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    private Pattern pattern;
+
+    public EmailInputTextValidator() {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if (value == null) {
+            return;
+        }
+
+        if (!pattern.matcher(value.toString()).matches()) {
+            ((InputText) component).setValid(false);
+        }
+    }
+}
