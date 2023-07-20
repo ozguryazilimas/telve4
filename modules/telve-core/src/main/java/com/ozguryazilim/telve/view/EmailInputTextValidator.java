39e2a93025bc0a709aac49f51bef9cc339c0dfe8
emrygun <emre6271@gmail.com>
2023-07-20 12:29:20 +0300

    refs #75002 simplified email validation method

--- modules/telve-core/src/main/java/com/ozguryazilim/telve/view/EmailInputTextValidator.java
+++ modules/telve-core/src/main/java/com/ozguryazilim/telve/view/EmailInputTextValidator.java
@@ -7,18 +7,12 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import java.util.regex.Pattern;
 
 @FacesValidator("emailInputTextValidator")
 public class EmailInputTextValidator implements Validator {
-    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-    private Pattern pattern;
-
-    public EmailInputTextValidator() {
-        pattern = Pattern.compile(EMAIL_PATTERN);
-    }
 
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
@@ -26,7 +20,10 @@ public class EmailInputTextValidator implements Validator {
             return;
         }
 
-        if (!pattern.matcher(value.toString()).matches()) {
+        try {
+            InternetAddress emailAddr = new InternetAddress((String) value);
+            emailAddr.validate();
+        } catch (AddressException e) {
             ((InputText) component).setValid(false);
         }
     }
